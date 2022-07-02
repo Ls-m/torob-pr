2ed0390d2c9f24dd38d0165cc358cc3c2f4f2229
Lham <elham.fr80@gmail.com>
2022-07-03 01:49:56 +0430

    some bugs fixed

--- torob-frontend/src/components/ProductCard.js
+++ torob-frontend/src/components/ProductCard.js
@@ -1,12 +1,59 @@
 
 import React from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import { useState } from 'react';
 
 
 const ProductCard = (props) =>{
     const a = props.value;
+    const [user,setUser] = useState({});
+    const [show,setShow] = useState(true);
+    const cookies = new Cookies();
+    const myToken = cookies.get('loginToken');
+    // if(props.note){
+    //     setShow(false);
+
+
+    // }
     //console.log(a.id);
     //const dispatch = useDispatch();
+    // useEffect(() => {
+    //     (async () => {
+           
+            
+
+    
+           
+    //     })();
+    //   }, []);
+
+    const addFavDb=async()=>{
+        
+        if(myToken){
+            console.log("fav clicked")
+            let answer = await axios.get('http://localhost:8080/api/users');
+            let allUsers = answer.data.users;;
+            const u = allUsers.find((user)=>user.token === myToken);
+            console.log(myToken)
+            if(u) {setUser(u);}
+            console.log(u.username)
+    
+            let Url = 'http://localhost:8080/api/users/updateFav/'+u.username
+                await axios.put(Url, {
+                    favorite:a.id,
+                    
+                }).then(function (response) {
+                    console.log(response);
+                  })
+                  .catch(function (error) {
+                    console.log("errrrr");
+                  });
+
+        }
+       
+    }
     return(
         <>
         {/* <div>{a.price}</div> */}
@@ -20,7 +67,7 @@ const ProductCard = (props) =>{
                     <Link to={`/products/${a.id}`} className='text-decoration-none text-dark'>
                         <h6>{a.name}</h6>
                     </Link>
-                    <button className="btn btn-outline-danger" type="button" >افزودن به علاقه‌مندی</button>
+                    <button className="btn btn-outline-danger" type="button" onClick={addFavDb}>افزودن به علاقه‌مندی</button>
                     {/* <a href="../pages/Product/" className='text-decoration-none text-dark'>
                         <h6>{a.title}</h6>
                     </a> */}
